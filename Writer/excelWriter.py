8283cf6de25bb99a1b0e5828b5b1bdc0f48ac212
Boxtell <48140541+Boxtell@users.noreply.github.com>
2021-02-03 14:01:38 +0100

    Add files via upload

--- Writer/excelWriter.py
+++ Writer/excelWriter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime as dt
 
 
 class ExcelWriter:
@@ -6,8 +7,9 @@ class ExcelWriter:
         pass
 
     @staticmethod
-    def df_to_excel(*args):
-        writer = pd.ExcelWriter('/Users/admin/Desktop/mkmd.xlsx', engine='xlsxwriter')
+    def df_to_excel(path, *args):
+        path = path + dt.datetime.today().strftime("%d_%m_%Y") + ".xlsx"
+        writer = pd.ExcelWriter(path, engine='xlsxwriter')
 
         for df in args:
             df.to_excel(excel_writer=writer, sheet_name=df.name)
